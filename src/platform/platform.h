a4018f4da691c5fde69044bac41da1016e0f3ed3
amoshi <amoshi.mandrakeuser@gmail.com>
2019-01-28 10:59:59 +0300

    change place for win32 compiler for tommyds lib, fix link directory for libuv

--- src/platform/platform.h
+++ src/platform/platform.h
@@ -2,33 +2,3 @@
 #ifdef __linux__
 #include <bsd/string.h>
 #endif
-#ifdef _WIN32
-#define SIGUSR1 0
-#define SIGUSR2 0
-#define SIGQUIT 0
-#define SIGTRAP 0
-#define SIGPIPE 0
-#define STDIN_FILENO 0
-#define STDOUT_FILENO 0
-#define STDERR_FILENO 0
-size_t
-strlcpy(char *dst, const char *src, size_t siz)
-{
-	char *d = dst;
-	const char *s = src;
-	size_t n = siz;
-	if (n != 0) {
-		while (--n != 0) {
-			if ((*d++ = *s++) == '\0')
-				break;
-		}
-	}
-	if (n == 0) {
-		if (siz != 0)
-			*d = '\0';
-		while (*s++)
-			;
-	}
-	return(s - src - 1);
-}
-#endif
