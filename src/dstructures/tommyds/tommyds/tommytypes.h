e410ac99ed203f428fa1d6293ea4ddc6f04f975c
amoshi <amoshi.mandrakeuser@gmail.com>
2019-01-30 21:24:27 +0300

    test

--- src/dstructures/tommyds/tommyds/tommytypes.h
+++ src/dstructures/tommyds/tommyds/tommytypes.h
@@ -30,6 +30,11 @@
 
 #ifndef __TOMMYTYPES_H
 #define __TOMMYTYPES_H
+#include <inttypes.h>
+#define _int64_t long long int
+#define _int64 long long int
+#define __int64 long long int
+#define __int32 int
 
 /******************************************************************************/
 /* types */
@@ -38,17 +43,12 @@
 #include <inttypes.h>
 
 #ifdef _MSC_VER
-#include <inttypes.h>
-#define _int64_t long long int
-#define _int64 long long int
-#define __int64 long long int
-#define __int32 int
 typedef unsigned tommy_uint32_t; /**< Generic uint32_t type. */
 typedef unsigned _int64 tommy_uint64_t; /**< Generic uint64_t type. */
 typedef size_t tommy_uintptr_t; /**< Generic uintptr_t type. */
 #ifdef _WIN64
 #define TOMMY_SIZE_BIT 64
-typedef unsigned int64_t tommy_size_t; /**< Generic size_t type. */
+typedef unsigned _int64_t tommy_size_t; /**< Generic size_t type. */
 typedef _int64_t tommy_ssize_t; /**< Generic ssize_t type. */
 #else
 #define TOMMY_SIZE_BIT 32
