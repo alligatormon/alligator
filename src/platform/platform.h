4252d7e01618da9b55eb6c0e592264e73aefb319
amoshi <amoshi.mandrakeuser@gmail.com>
2019-01-31 11:29:47 +0300

    test

--- src/platform/platform.h
+++ src/platform/platform.h
@@ -12,10 +12,30 @@
 #define _int64 long long int
 #define __int64 long long int
 #define __int32 int
-//char* strndup(char *str, size_t sz)
-//{
-//	char *ret = malloc(sz);
-//	strlcpy(ret, str, sz);
-//	return ret;
-//}
+size_t
+strlcpy(char *dst, const char *src, size_t siz)
+{
+       char *d = dst;
+       const char *s = src;
+       size_t n = siz;
+       if (n != 0) {
+               while (--n != 0) {
+                       if ((*d++ = *s++) == '\0')
+                               break;
+               }
+       }
+       if (n == 0) {
+               if (siz != 0)
+                       *d = '\0';
+               while (*s++)
+                       ;
+       }
+       return(s - src - 1);
+}
+char* strndup(char *str, size_t sz)
+{
+	char *ret = malloc(sz);
+	strlcpy(ret, str, sz);
+	return ret;
+}
 #endif
