aff02a1c1c4af64f1d48d34fe5442662447023b8
amoshi <amoshi.mandrakeuser@gmail.com>
2019-02-03 21:03:52 +0300

    add scrape system metrics periodicly

--- src/system/windows.c
+++ src/system/windows.c
@@ -2,8 +2,15 @@
 #include <windows.h>
 #include <stdio.h>
 #include <psapi.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
 #pragma comment(lib, "user32.lib")
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
 
+#define MALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
+#define FREE(x) HeapFree(GetProcessHeap(), 0, (x))
 
 double get_process_name( DWORD processID )
 {
@@ -134,6 +141,131 @@ void getprocessinfo()
     }
 
     return;
+}
+
+void get_network_stats()
+{
+    PMIB_TCPTABLE2 pTcpTable;
+    ULONG ulSize = 0;
+    DWORD dwRetVal = 0;
+
+    char szLocalAddr[128];
+    char szRemoteAddr[128];
+
+    struct in_addr IpAddr;
+
+    int i;
+
+    pTcpTable = (MIB_TCPTABLE2 *) MALLOC(sizeof (MIB_TCPTABLE2));
+    if (pTcpTable == NULL) {
+        printf("Error allocating memory\n");
+        return 1;
+    }
+
+    ulSize = sizeof (MIB_TCPTABLE);
+// Make an initial call to GetTcpTable2 to
+// get the necessary size into the ulSize variable
+    if ((dwRetVal = GetTcpTable2(pTcpTable, &ulSize, TRUE)) ==
+        ERROR_INSUFFICIENT_BUFFER) {
+        FREE(pTcpTable);
+        pTcpTable = (MIB_TCPTABLE2 *) MALLOC(ulSize);
+        if (pTcpTable == NULL) {
+            printf("Error allocating memory\n");
+            return 1;
+        }
+    }
+// Make a second call to GetTcpTable2 to get
+// the actual data we require
+    if ((dwRetVal = GetTcpTable2(pTcpTable, &ulSize, TRUE)) == NO_ERROR) {
+        printf("\tNumber of entries: %d\n", (int) pTcpTable->dwNumEntries);
+        for (i = 0; i < (int) pTcpTable->dwNumEntries; i++) {
+            printf("\n\tTCP[%d] State: %ld - ", i,
+                   pTcpTable->table[i].dwState);
+            switch (pTcpTable->table[i].dwState) {
+            case MIB_TCP_STATE_CLOSED:
+                printf("CLOSED\n");
+                break;
+            case MIB_TCP_STATE_LISTEN:
+                printf("LISTEN\n");
+                break;
+            case MIB_TCP_STATE_SYN_SENT:
+                printf("SYN-SENT\n");
+                break;
+            case MIB_TCP_STATE_SYN_RCVD:
+                printf("SYN-RECEIVED\n");
+                break;
+            case MIB_TCP_STATE_ESTAB:
+                printf("ESTABLISHED\n");
+                break;
+            case MIB_TCP_STATE_FIN_WAIT1:
+                printf("FIN-WAIT-1\n");
+                break;
+            case MIB_TCP_STATE_FIN_WAIT2:
+                printf("FIN-WAIT-2 \n");
+                break;
+            case MIB_TCP_STATE_CLOSE_WAIT:
+                printf("CLOSE-WAIT\n");
+                break;
+            case MIB_TCP_STATE_CLOSING:
+                printf("CLOSING\n");
+                break;
+            case MIB_TCP_STATE_LAST_ACK:
+                printf("LAST-ACK\n");
+                break;
+            case MIB_TCP_STATE_TIME_WAIT:
+                printf("TIME-WAIT\n");
+                break;
+            case MIB_TCP_STATE_DELETE_TCB:
+                printf("DELETE-TCB\n");
+                break;
+            default:
+                printf("UNKNOWN dwState value\n");
+                break;
+            }
+            IpAddr.S_un.S_addr = (u_long) pTcpTable->table[i].dwLocalAddr;
+            strcpy_s(szLocalAddr, sizeof (szLocalAddr), inet_ntoa(IpAddr));
+            printf("\tTCP[%d] Local Addr: %s\n", i, szLocalAddr);
+            printf("\tTCP[%d] Local Port: %d \n", i,
+                   ntohs((u_short)pTcpTable->table[i].dwLocalPort));
+
+            IpAddr.S_un.S_addr = (u_long) pTcpTable->table[i].dwRemoteAddr;
+            strcpy_s(szRemoteAddr, sizeof (szRemoteAddr), inet_ntoa(IpAddr));
+            printf("\tTCP[%d] Remote Addr: %s\n", i, szRemoteAddr);
+            printf("\tTCP[%d] Remote Port: %d\n", i,
+                   ntohs((u_short)pTcpTable->table[i].dwRemotePort));
+
+            printf("\tTCP[%d] Owning PID: %d\n", i, pTcpTable->table[i].dwOwningPid);
+            printf("\tTCP[%d] Offload State: %ld - ", i,
+                   pTcpTable->table[i].dwOffloadState);
+            switch (pTcpTable->table[i].dwOffloadState) {
+            case TcpConnectionOffloadStateInHost:
+                printf("Owned by the network stack and not offloaded \n");
+                break;
+            case TcpConnectionOffloadStateOffloading:
+                printf("In the process of being offloaded\n");
+                break;
+            case TcpConnectionOffloadStateOffloaded:
+                printf("Offloaded to the network interface control\n");
+                break;
+            case TcpConnectionOffloadStateUploading:
+                printf("In the process of being uploaded back to the network stack \n");
+                break;
+            default:
+                printf("UNKNOWN Offload state value\n");
+                break;
+            }
+
+        }
+    } else {
+        printf("\tGetTcpTable2 failed with %d\n", dwRetVal);
+        FREE(pTcpTable);
+        return 1;
+    }
+
+    if (pTcpTable != NULL) {
+        FREE(pTcpTable);
+        pTcpTable = NULL;
+    }
 }
 
 void get_system_metrics()
