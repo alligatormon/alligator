59f5630a7f9a75812610c9890b47fed128b268dd
amoshi <amoshi.mandrakeuser@gmail.com>
2019-02-03 22:56:33 +0300

    add scrape system metrics periodicly

--- src/system/windows.c
+++ src/system/windows.c
@@ -144,6 +144,47 @@ void getprocessinfo()
     return;
 }
 
+void get_network_counters()
+{
+    PMIB_TCPSTATS pTCPStats;
+    DWORD dwRetVal = 0;
+
+    pTCPStats = (MIB_TCPSTATS*) MALLOC (sizeof(MIB_TCPSTATS));
+    if (pTCPStats == NULL) {
+        printf("Error allocating memory\n");
+        return 1;
+    }
+
+    if ((dwRetVal = GetTcpStatistics(pTCPStats)) == NO_ERROR) {
+      printf("\tActive Opens: %ld\n", pTCPStats->dwActiveOpens);
+      printf("\tPassive Opens: %ld\n", pTCPStats->dwPassiveOpens);
+      printf("\tSegments Recv: %ld\n", pTCPStats->dwInSegs);
+      printf("\tSegments Xmit: %ld\n", pTCPStats->dwOutSegs);
+      printf("\tTotal # Conxs: %ld\n", pTCPStats->dwNumConns);
+    }
+    else {
+      printf("GetTcpStatistics failed with error: %ld\n", dwRetVal);
+      
+      LPVOID lpMsgBuf;
+      if (FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_FROM_SYSTEM | 
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dwRetVal,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+        (LPTSTR) &lpMsgBuf,
+        0,
+        NULL )) {
+        printf("\tError: %s", lpMsgBuf);
+      }
+      LocalFree( lpMsgBuf );
+    }
+
+    if (pTCPStats)
+        FREE (pTCPStats);
+}
+}
+
 void get_network_stats()
 {
     PMIB_TCPTABLE2 pTcpTable;
@@ -310,5 +351,6 @@ void get_system_metrics()
 
 	//getprocessinfo();
 	get_network_stats();
+	get_network_counters();
 }
 #endif
