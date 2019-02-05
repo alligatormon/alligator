1e791555119cf431dde396a1822587dbd59049cb
amoshi <amoshi.mandrakeuser@gmail.com>
2019-02-05 22:51:02 +0300

    add windows sql server monitoring

--- src/parsers/sqlserver.c
+++ src/parsers/sqlserver.c
@@ -31,80 +31,75 @@ void sqlserver_handler()
 	SQLRETURN retcode;
 	puts("5");
 
-	do
-	{
 	puts("6");
-		if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlenvhandle))
-			break;
+	if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlenvhandle))
+		return;
 	puts("7");
-		if(SQL_SUCCESS!=SQLSetEnvAttr(sqlenvhandle,SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0)) 
-			break;
+	if(SQL_SUCCESS!=SQLSetEnvAttr(sqlenvhandle,SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0)) 
+		return;
 	puts("8");
-		if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_DBC, sqlenvhandle, &sqlconnectionhandle))
-			break;
+	if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_DBC, sqlenvhandle, &sqlconnectionhandle))
+		return;
 	puts("9");
-		SQLCHAR retconstring[1024];
+	SQLCHAR retconstring[1024];
 	puts("10");
 
-		switch(SQLDriverConnect (sqlconnectionhandle, NULL, 
-				(SQLCHAR*)"DRIVER={SQL Server};SERVER=localhost, 1433;DATABASE=test;UID=test;PWD=test;", 
-				SQL_NTS, retconstring, 1024, NULL,SQL_DRIVER_NOPROMPT))
-		{
-			case SQL_SUCCESS_WITH_INFO:
-				show_error(SQL_HANDLE_DBC, sqlconnectionhandle);
+	switch(SQLDriverConnect (sqlconnectionhandle, NULL, 
+			(SQLCHAR*)"DRIVER={SQL Server};SERVER=localhost, 1433;DATABASE=test;UID=test;PWD=test;", 
+			SQL_NTS, retconstring, 1024, NULL,SQL_DRIVER_NOPROMPT))
+	{
+		case SQL_SUCCESS_WITH_INFO:
+			show_error(SQL_HANDLE_DBC, sqlconnectionhandle);
 	puts("11");
-				break;
-			case SQL_INVALID_HANDLE:
-			case SQL_ERROR:
-				show_error(SQL_HANDLE_DBC, sqlconnectionhandle);
-				retcode = -1;
+			return;
+		case SQL_INVALID_HANDLE:
+		case SQL_ERROR:
+			show_error(SQL_HANDLE_DBC, sqlconnectionhandle);
+			retcode = -1;
 	puts("12");
-				break;
-			default:
+			return;
+		default:
 	puts("13");
-				break;
-		}
+			return;
+	}
 	puts("14");
 
-		if(retcode == -1)
-			break;
+	if(retcode == -1)
+		return;
 	puts("15");
-		if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_STMT, sqlconnectionhandle, &sqlstatementhandle))
-			break;
+	if(SQL_SUCCESS!=SQLAllocHandle(SQL_HANDLE_STMT, sqlconnectionhandle, &sqlstatementhandle))
+		return;
 	puts("16");
-		if(SQL_SUCCESS!=SQLExecDirect(sqlstatementhandle, (SQLCHAR*)"select * from testtable", SQL_NTS))
-		{
-			show_error(SQL_HANDLE_STMT, sqlstatementhandle);
-			break;
+	if(SQL_SUCCESS!=SQLExecDirect(sqlstatementhandle, (SQLCHAR*)"select * from testtable", SQL_NTS))
+	{
+		show_error(SQL_HANDLE_STMT, sqlstatementhandle);
+		return;
 	puts("17");
-		}
-		else
-		{
-	puts("18");
-			char name[64];
-	puts("19");
-			char address[64];
-	puts("20");
-			int id;
-	puts("21");
-			while(SQLFetch(sqlstatementhandle)==SQL_SUCCESS)
-			{
-	puts("22");
-				SQLGetData(sqlstatementhandle, 1, SQL_C_ULONG, &id, 0, NULL);
-	puts("23");
-				SQLGetData(sqlstatementhandle, 2, SQL_C_CHAR, name, 64, NULL);
-	puts("24");
-				SQLGetData(sqlstatementhandle, 3, SQL_C_CHAR, address, 64, NULL);
-	puts("25");
-				printf("id %s name %s address %s\n", id, name, address);
-	puts("26");
-			}
-	puts("27");
-		}
-	puts("28");
 	}
-	while(FALSE);
-	puts("29");
+	else
+	{
+	puts("18");
+		char name[64];
+	puts("19");
+		char address[64];
+	puts("20");
+		int id;
+	puts("21");
+		while(SQLFetch(sqlstatementhandle)==SQL_SUCCESS)
+		{
+	puts("22");
+			SQLGetData(sqlstatementhandle, 1, SQL_C_ULONG, &id, 0, NULL);
+	puts("23");
+			SQLGetData(sqlstatementhandle, 2, SQL_C_CHAR, name, 64, NULL);
+	puts("24");
+			SQLGetData(sqlstatementhandle, 3, SQL_C_CHAR, address, 64, NULL);
+	puts("25");
+			printf("id %s name %s address %s\n", id, name, address);
+	puts("26");
+		}
+	puts("27");
+	}
+	puts("28");
 	SQLFreeHandle(SQL_HANDLE_STMT, sqlstatementhandle );
 	puts("30");
 	SQLDisconnect(sqlconnectionhandle);
