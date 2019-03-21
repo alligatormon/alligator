eea61316625c293aee0f4d66699a2999f98a415d
amoshi <amoshi.mandrakeuser@gmail.com>
2019-03-22 02:15:42 +0300

    stage tests in CI

---
+++ src/events/client_info.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <uv.h>
+#include "common/rtime.h"
+#include "dstructures/tommyds/tommyds/tommy.h"
+typedef struct client_info
+{
+	struct sockaddr_in *dest;
+	uv_connect_t *connect;
+	uv_tcp_t *socket;
+	char *key;
+	void *parser_handler;
+	char *mesg;
+	char *hostname;
+	char *port;
+	r_time connect_time;
+	r_time connect_time_finish;
+	r_time write_time;
+	r_time write_time_finish;
+	r_time read_time;
+	r_time read_time_finish;
+	int lock;
+	int proto;
+
+	tommy_node node;
+} client_info;
