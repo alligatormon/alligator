69db40f47d716751a37955c451a1fbf58ab574f1
amoshi <amoshi.mandrakeuser@gmail.com>
2019-03-29 21:36:56 +0300

    update network sys

--- src/common/aggregator.c
+++ src/common/aggregator.c
@@ -14,8 +14,26 @@ void smart_aggregator_selector(host_aggregator_info *hi, void *handler, char *me
 	}
 	else if (hi->proto == APROTO_TCP)
 	{
-		//do_unix_client(hi->host, handler, mesg);
 		do_tcp_client(hi->host, hi->port, handler, mesg);
+	}
+	else if (hi->proto == APROTO_UNIXGRAM)
+	{
+		do_unixgram_client(hi->host, handler, mesg);
+	}
+}
+
+void smart_aggregator_selector_buffer(host_aggregator_info *hi, void *handler, uv_buf_t *buffer, size_t buflen)
+{
+	if (!hi)
+		return;
+
+	if (hi->proto == APROTO_UNIX)
+	{
+		do_unix_client_buffer(hi->host, handler, buffer, buflen);
+	}
+	else if (hi->proto == APROTO_TCP)
+	{
+		do_tcp_client_buffer(hi->host, hi->port, handler, buffer, buflen);
 	}
 }
 
@@ -32,5 +50,9 @@ void smart_aggregator_selector_plain(int proto, char *hostname, char *port, void
 	else if (proto == APROTO_TCP)
 	{
 		do_tcp_client(hostname, port, handler, mesg);
+	}
+	else if (proto == APROTO_UNIXGRAM)
+	{
+		do_unixgram_client(hostname, handler, mesg);
 	}
 }
