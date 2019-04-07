35048250a8ec41ff59f8574a5ef1bcd48e5bb60c
amoshi <amoshi.mandrakeuser@gmail.com>
2019-04-07 07:57:28 +0300

    update http query module

--- src/common/http.c
+++ src/common/http.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-char* gen_http_query(int http_type, char *method_query, char *host, char *useragent, char *auth)
+char* gen_http_query(int http_type, char *method_query, char *host, char *useragent, char *auth, int clrf)
 {
 	size_t size_useragent, size_auth, size_method_query;
 	if ( method_query )
@@ -34,8 +34,16 @@ char* gen_http_query(int http_type, char *method_query, char *host, char *userag
 		default: strlcpy(method, "GET", 4);
 	}
 
-	if (auth)	snprintf(buf, sz, "%s %s HTTP/1.1\nHost: %s\nUser-Agent: %s\nAuthorization: Basic %s\n\n", method, method_query, host, useragent, auth);
-	else		snprintf(buf, sz, "%s %s HTTP/1.1\nHost: %s\nUser-Agent: %s\n\n", method, method_query, host, useragent);
+	if (clrf)
+	{
+		if (auth)	snprintf(buf, sz, "%s %s HTTP/1.1\r\nHost: %s\r\nUser-Agent: %s\r\nAuthorization: Basic %s\r\n\r\n", method, method_query, host, useragent, auth);
+		else		snprintf(buf, sz, "%s %s HTTP/1.1\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n", method, method_query, host, useragent);
+	}
+	else
+	{
+		if (auth)	snprintf(buf, sz, "%s %s HTTP/1.1\nHost: %s\nUser-Agent: %s\nAuthorization: Basic %s\n\n", method, method_query, host, useragent, auth);
+		else		snprintf(buf, sz, "%s %s HTTP/1.1\nHost: %s\nUser-Agent: %s\n\n", method, method_query, host, useragent);
+	}
 
 	return buf;
 }
