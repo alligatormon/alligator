610014338166e6bd06669850a1e60c383ab149a0
amoshi <amoshi.mandrakeuser@gmail.com>
2020-10-25 15:44:51 +0300

    add support mongodb and postgresql

--- src/tests/system/mongodb/run.sh
+++ src/tests/system/mongodb/run.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 [ -z "$1" ] && APPDIR="/app/src/" || APPDIR="$1"
-$APPDIR/alligator $APPDIR/tests/system/mongodb/alligator.conf&
+. /app/src/tests/system/common.sh
 
-mongod --dbpath /mongo --fork --syslog
-$APPDIR/tests/system/mongodb/inserts.mongo
+DIR="mongodb"
+mongod --fork --syslog
+#$APPDIR/tests/system/mongodb/inserts.mongo
+
+$APPDIR/alligator $APPDIR/tests/system/$DIR/alligator.conf&
+sleep 15
+
+TEXT=`curl -s localhost:1111`
+METRICS=`cat $APPDIR/tests/system/$DIR/metrics.txt`
+for NAME in $METRICS
+do
+	echo "$TEXT" | grep $NAME >/dev/null 2>&1 && success $NAME || error "$TEXT" $NAME
+done
 
 curl localhost:1111
 kill %1
+pkill mongod
--- src/tests/system/postgresql/run.sh
+++ src/tests/system/postgresql/run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 [ -z "$1" ] && APPDIR="/app/src/" || APPDIR="$1"
-$APPDIR/alligator $APPDIR/tests/system/postgresql/alligator.conf&
 
-mkdir /postgres
-chown postgres:postgres /postgres
-su postgres -c "initdb -D /postgres"
-su postgres -c "pg_ctl -D /postgres -l logfile start"
+su postgres -c 'initdb -D /tmp/pg'
+su postgres -c 'pg_ctl -D /tmp/pg -l logfile start'
+
+$APPDIR/alligator $APPDIR/tests/system/postgresql/alligator.conf&
+sleep 15
 
 curl localhost:1111
 kill %1
+su postgres -c 'pg_ctl -D /tmp/pg -l logfile stop'
