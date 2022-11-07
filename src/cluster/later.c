a4a6d45eec2332b0b06fa7acc4636f4e94bdf760
amoshi <amoshi.mandrakeuser@gmail.com>
2022-11-07 09:45:37 +0300

    update cluster support, update pgsql/mysql support, add metrics for system_scopes count, add timeout for exec commands use, fix memleaks

---
+++ src/cluster/later.c
@@ -0,0 +1,31 @@
+#include "events/context_arg.h"
+#include "cluster/get.h"
+
+int cluster_come_later(context_arg *carg)
+{
+	if (!carg->cluster)
+		return 0;
+
+	cluster_node *cn = carg->cluster_node;
+	if (!cn)
+		cn = cluster_get(carg->cluster);
+
+	if (!cn)
+		return 0;
+
+	printf("LATER update_count is %llu\n", cn->update_count);
+
+	if (cn->update_count < (cn->servers_size * 8))
+		return 0;
+
+	r_time time_now = setrtime();
+
+	printf("cluster_come_later check: '%s':'%s' && %d %d, result: %d\n", cn->shared_lock_instance ? cn->shared_lock_instance->s : NULL, carg->instance, (cn->ttl >= time_now.sec), carg->parser_status, ((string_compare(cn->shared_lock_instance, carg->instance, strlen(carg->instance))) && (cn->ttl >= time_now.sec)));
+	if ((string_compare(cn->shared_lock_instance, carg->instance, strlen(carg->instance))) && (cn->ttl >= time_now.sec))
+		return 1;
+
+	cn->parser_status = carg->parser_status;
+	//printf("SET parser_status to %d\n", cn->parser_status);
+
+	return 0;
+}
---
+++ src/router/sharedlock.c
@@ -0,0 +1,62 @@
+#include <jansson.h>
+#include "common/selector.h"
+#include "events/context_arg.h"
+#include "parsers/http_proto.h"
+#include "metric/namespace.h"
+#include "common/http.h"
+#include "cluster/type.h"
+#include <stdlib.h>
+#define SHAREDLOCK_RESPONSE_HEADERS "HTTP/1.1 200 OK\r\nServer: alligator\r\nContent-Type: text/plain\r\nConnection: close\r\n"
+
+void sharedlock_api_response(string *response, string *body)
+{
+	char *content_length = malloc(255);
+	uint64_t size = body ? body->l : 0;
+	snprintf(content_length, 255, "Content-Length: %zu\r\n\r\n", size);
+
+	string_cat(response, SHAREDLOCK_RESPONSE_HEADERS, strlen(SHAREDLOCK_RESPONSE_HEADERS));
+	string_cat(response, content_length, strlen(content_length));
+
+	if (body)
+	{
+		string_string_cat(response, body);
+		string_free(body);
+	}
+
+	free(content_length);
+}
+
+void sharedlock_get_router(string *response, http_reply_data* http_data, context_arg *carg)
+{
+	alligator_ht *args = http_get_args(http_data->uri, http_data->uri_size);
+
+	char *name = http_get_param(args, "name");
+
+	string *body = cluster_get_sharedlock(name);
+
+	sharedlock_api_response(response, body);
+
+	http_args_free(args);
+}
+
+
+void sharedlock_post_router(string *response, http_reply_data* http_data, context_arg *carg)
+{
+	alligator_ht *args = http_get_args(http_data->uri, http_data->uri_size);
+
+	char *name = http_get_param(args, "name");
+	char *replica = http_get_param(args, "replica");
+
+	string *body = cluster_set_sharedlock(replica, name, carg);
+
+	sharedlock_api_response(response, body);
+
+	http_args_free(args);
+
+	if (carg->rreturn == ENTRYPOINT_RETURN_EMPTY)
+	{
+		string_null(response);
+		string_cat(response, SHAREDLOCK_RESPONSE_HEADERS, strlen(SHAREDLOCK_RESPONSE_HEADERS));
+		string_cat(response, "\r\n", 2);
+	}
+}
