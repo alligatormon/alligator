f3dcda88ee93ca3b11e37ea55b1a080643f8acf6
amoshi <amoshi.mandrakeuser@gmail.com>
2024-04-02 09:43:35 +0300

    remove legacy alligator_FTW

--- src/common/rpm.h
+++ src/common/rpm.h
@@ -1,37 +1,6 @@
 #pragma once
 #ifdef __linux__
  
-#include <rpm/rpmlib.h>
-#include <rpm/rpmds.h>
-#include <rpm/rpmts.h>
-#include <rpm/rpmdb.h>
-
-typedef struct rpm_library
-{
-	void (*rpmReadConfigFiles)(void*, void*);
-	uv_lib_t *rpmReadConfigFiles_lib;
-
-	rpmts (*rpmtsCreate)();
-	uv_lib_t *rpmtsCreate_lib;
-
-	Header (*rpmdbNextIterator)(rpmdbMatchIterator);
-	uv_lib_t *rpmdbNextIterator_lib;
-
-	rpmdbMatchIterator (*rpmtsInitIterator)(rpmts, int, void*, int);
-	uv_lib_t *rpmtsInitIterator_lib;
-
-	void (*headerGetEntry)(Header, int, unsigned*, void**, unsigned*);
-	uv_lib_t *headerGetEntry_lib;
-
-	void (*rpmtsFree)(rpmts);
-	uv_lib_t *rpmtsFree_lib;
-
-	void (*rpmdbFreeIterator)(rpmdbMatchIterator);
-	uv_lib_t *rpmdbFreeIterator_lib;
-
-} rpm_library;
-
-void rpm_library_init();
 void get_rpm_info();
 
 #endif
