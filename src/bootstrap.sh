b2ee92c12c04206ad4a169a83274c589098932df
amoshi <amoshi.mandrakeuser@gmail.com>
2024-07-08 09:55:55 +0300

    some fixes

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -1,10 +1,5 @@
 export DEBIAN_FRONTEND=noninteractive
 
-#centos 7
-sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
-sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
-sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
-
 apt update
 apt -y install software-properties-common
 apt -y install gpg
