fca684c4ec0c8419c2ba9101084106186df08d2b
amoshi <amoshi.mandrakeuser@gmail.com>
2026-03-10 10:03:46 +0300

    add support for /cluster endpoint in patroni to receive lag statistics

--- src/external/conanfile.py
+++ src/external/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile
+from conan import ConanFile
 from platform import system
 
 class Deps(ConanFile):
@@ -6,9 +6,11 @@ class Deps(ConanFile):
 
     def build(self):
         if system() != "FreeBSD":
-            self.run("cd external/libfyaml && find . -type f -exec sed -i -e 's/XXH/libfyaml_XXH/g'  -e 's/libfyaml_libfyaml_XXH/libfyaml_XXH/g' {} \; && sh bootstrap.sh && autoreconf -f && autoreconf -i -f && ./configure && make -j4 && make -j4")
+            self.run("cd external/libfyaml && find . -type f -exec sed -i -e 's/XXH/libfyaml_XXH/g' -e 's/libfyaml_libfyaml_XXH/libfyaml_XXH/g' {} \; && sh bootstrap.sh && autoreconf -f && autoreconf -i -f && ./configure && make -j4 && make -j4")
+
         if system() != "FreeBSD":
             self.run("cd external/libatasmart && ./autogen.sh && ./configure --enable-static && make -j4 && make -j4")
+
         self.run("cd external/mruby && make")
         self.run("cd external/libhv && ./configure --without-http-server --without-http-client --without-http --without-evpp && make libhv")
         self.run("cd external/libhv && cc -c -Iinclude/hv/ -std=c99 -o libdns.o protocol/dns.c && ar rcs lib/libdns.a libdns.o")
